8141a73c0837ed3cceaa0a773140b78045015037
Trung Doan <trungict1994@gmail.com>
2016-06-11 21:17:37 +0700

    First commit on Github

---
+++ ajax.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Quản lý tiến trình khi gửi ajax</title>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <script language="javascript" src="http://code.jquery.com/jquery-2.0.0.min.js"></script>
+    </head>
+     
+    <body>
+       
+        <script language="javascript">
+            var i=0;
+          var timeout = setTimeout(function(){
+			  i+=1;
+		writeln(i);
+	}, 1000);
+        </script>
+    </body>
+</html>
---
+++ ajax1.php
@@ -0,0 +1,31 @@
+<?php
+$json = '
+[
+    {
+        "username":"thehalfheart",
+        "email":"thehalfheart@gmail.com"
+    },
+    {
+        "username":"nvcuongsp",
+        "email":"thehalfheart@gmail.com"
+    },
+    {
+        "username":"mrcuongwinter",
+        "email":"thehalfheart@gmail.com"
+    },
+    {
+        "username":"ftuts",
+        "email":"thehalfheart@gmail.com"
+    },
+    {
+        "username":"freetutorials",
+        "email":"thehalfheart@gmail.com"
+    }
+]';
+ 
+// Ngủ 5 giây để test ajax
+sleep(1);
+ 
+// Xuất json
+echo $json;
+?>
